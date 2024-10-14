9c2ba590da2899a7d34a432ed54d4a32c7e8c05f
Lectival <lectival857@gmail.com>
2024-10-14 20:41:25 +0700

    add solution for https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/

---
+++ leetcode/sql/0586_customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,11 @@
+select customer_number
+from Orders
+group by customer_number
+having count(order_number) = (
+    select max(order_count)
+    from (
+        select count(order_number) as order_count
+        from Orders
+        group by customer_number
+    ) as order_count
+);
